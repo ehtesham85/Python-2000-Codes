f6b9c4bde016d4598cb8174dd773ca73763360fa
Ehtesham Jutt <ehtesham8586@gmail.com>
2025-07-19 20:23:03 +0500

    Add files via upload

---
+++ 7_List-Tuple-Dictionary.py
@@ -0,0 +1,24 @@
+a = complex(8, 2)
+b = True
+c = "hello"
+d = None
+e = 10
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is", type(d))
+print("The type of e is", type(e))
+
+list1 = [8, 2.3, [-4, 5], ["Apple", "banana"]]
+print(list1)
+tuple1 = (("parrot", "sparrow"), ("lion", "tiger"))
+print(tuple1)
+dict1 = {"name": "Sakshi", "age": 20, "can vote": True}
+print(dict1)
