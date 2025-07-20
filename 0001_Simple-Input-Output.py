701a2dc1a738e90e5057f3e2f363734a88f624dc
MIHAMMAD EHTESHAM <ehtesham8586@gmail.com>
2025-07-20 09:04:24 +0500

    Add files via upload

---
+++ 0001_Simple-Input-Output.py
@@ -0,0 +1,11 @@
+print("Hey! Ehtesham")
+name="Ehtesham"
+age=21
+print("Name : ",name)
+print("Age : ",age)
+
+name=input("What is your name ?")
+
+age=input("Enter Your your age : ")
+print("Name : ",name) 
+print("Age : ",age)
