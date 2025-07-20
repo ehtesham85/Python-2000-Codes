9bd20ac17dd05829f35dfbe02d80d11cd9d6432e
MIHAMMAD EHTESHAM <ehtesham8586@gmail.com>
2025-07-20 09:06:31 +0500

    Add files via upload

---
+++ 0008_Functions.py
@@ -0,0 +1,39 @@
+a = 10
+b = 20
+
+
+def greet():
+  print("Assalam-u-Alaikum")
+
+
+def Listen():
+  print("I am Listening")
+
+
+def Speak():
+  print("I am Speaking")
+
+
+def add():
+  print("a+b", a + b)
+
+
+def sub():
+  print("a-b", a - b)
+
+
+def mul():
+  print("a*b", a * b)
+
+
+def div():
+  print("a/b", a / b)
+
+
+greet()
+Listen()
+Speak()
+add()
+sub()
+mul()
+div()
