f56831e326d00dc22d5ac5a8d85aefff99878d0f
MIHAMMAD EHTESHAM <ehtesham8586@gmail.com>
2025-07-20 09:05:33 +0500

    Add files via upload

---
+++ 0005_Comments-EscapeStatement.py
@@ -0,0 +1,15 @@
+  """
+  Hey! I'm writing this code so Please don't copy it.
+  Author: Ehtesham
+  Course: Python
+  """
+
+  print('Hey I am\' a \'good\"\nand this viewer is also a good boy/girl')
+
+  print("Hello Wordl")
+  # this is a single line comment 
+  # print("This is a print statement")
+  # print(Hello World !!!) # This is a comment
+
+  print("Hey" , 6 , 7, sep="&",end="200\n")
+  print("Ehtesham")
