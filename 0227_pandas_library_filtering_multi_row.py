71f316fa7f91a353a7d2383843aa449013a4c2d6
MIHAMMAD EHTESHAM <ehtesham8586@gmail.com>
2025-08-19 13:01:50 +0500

    Add files via upload

---
+++ 0227_pandas_library_filtering_multi_row.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+data = {
+    'Name': ['John', 'Anna', 'Peter', 'Linda', 'Ali', 'Ahmad', 'Ehtesham', 'Gulshan'],
+    'Age': [28, 24, 35, 32, 40, 45, 47, 39],
+    'City': ['New York', 'Paris', 'Berlin', 'London', 'Islamabad', 'Karachi', 'Lahore', 'Peshawar']
+}
+
+df = pd.DataFrame(data)
+print("Original DataFrame : ")
+print(df)
+print(f"Shape : {df.shape}")              # print rows and columns
+print(f"Columns Names : {df.columns}")    # print column names
+
+
+
+
+
+
+
+
